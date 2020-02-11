d47debf8e297a24bc411ff9a6ef9e1547399d754
Machado <elitonmachadod200@gmail.com>
2020-02-11 14:28:34 -0300

    add Processing MainLoop

---
+++ src/com/machado/view/MainLoop.java
@@ -0,0 +1,21 @@
+package com.machado.view;
+
+import processing.core.PApplet;
+
+public class MainLoop extends PApplet {
+
+    @Override
+    public void settings() {
+        size(1366, 768);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        background(255);
+    }
+}
